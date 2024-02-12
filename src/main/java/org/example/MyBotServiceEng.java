8803c0eb942479b687d49be7f8b543b6133797ff
Abduqodir <abduqodirxojiakbarov202gmail.com>
2024-02-12 14:11:40 +0500

    add MyBotServiceChn update class

--- src/main/java/org/example/MyBotServiceEng.java
+++ src/main/java/org/example/MyBotServiceEng.java
@@ -16,7 +16,7 @@ public class MyBotServiceEng {
     public SendMessage asosiyMenuEng(Long chatId){
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
-        sendMessage.setText("Asosiy Menu");
+        sendMessage.setText("Main menu");
         ReplyKeyboardMarkup reply = new ReplyKeyboardMarkup();
         List<KeyboardRow> rowList = new ArrayList<>();
         KeyboardRow row = new KeyboardRow();
@@ -26,36 +26,36 @@ public class MyBotServiceEng {
         KeyboardRow row4 = new KeyboardRow();
         KeyboardRow row5 = new KeyboardRow();
         KeyboardButton button = new KeyboardButton();
-        button.setText("\uD83D\uDE9AXizmatlar");
+        button.setText("\uD83D\uDE9AServices");
         row.add(button);
         rowList.add(row);
         KeyboardButton button1 = new KeyboardButton();
-        button1.setText("\uD83D\uDE99Xitoydan mashinalar");
+        button1.setText("\uD83D\uDE99Cars from China");
         row1.add(button1);
         rowList.add(row1);
         KeyboardButton button2 = new KeyboardButton();
-        button2.setText("⏰Yetkazib berish muddati");
+        button2.setText("⏰Delivery time");
         row2.add(button2);
         KeyboardButton button3 = new KeyboardButton();
-        button3.setText("☎\uFE0FBiz bilan bog'lanish");
+        button3.setText("☎\uFE0Fcontact us");
         row2.add(button3);
         rowList.add(row2);
         KeyboardButton button4 = new KeyboardButton();
-        button4.setText("\uD83C\uDFDBValyuta kursi");
+        button4.setText("\uD83C\uDFDBExchange rate");
         row3.add(button4);
         KeyboardButton button5 = new KeyboardButton();
-        button5.setText("\uD83E\uDDEEKalkulyator");
+        button5.setText("\uD83E\uDDEECalculator");
         row3.add(button5);
         rowList.add(row3);
         KeyboardButton button6 = new KeyboardButton();
-        button6.setText("ℹ\uFE0FBiz haqimizda");
+        button6.setText("ℹ\uFE0Fabout Us");
         row4.add(button6);
         KeyboardButton button7 = new KeyboardButton();
-        button7.setText("\uD83D\uDCACIzoh qoldirish");
+        button7.setText("\uD83D\uDCACLeave a comment");
         row4.add(button7);
         rowList.add(row4);
         KeyboardButton button8 = new KeyboardButton();
-        button8.setText("⚙\uFE0FSozlamalar");
+        button8.setText("⚙\uFE0FSettings");
         row5.add(button8);
         rowList.add(row5);
         reply.setKeyboard(rowList);
@@ -68,7 +68,7 @@ public class MyBotServiceEng {
         sendMessage.setChatId(chatId);
         sendMessage.setText("\uD83D\uDCDE Enter your phone number to register. \n" +
                 " \n" +
-                " Send the number as +1***********.");
+                " Send the number as +998***********.");
         ReplyKeyboardMarkup reply = new ReplyKeyboardMarkup();
 
         List<KeyboardRow> rows = new ArrayList<>();
