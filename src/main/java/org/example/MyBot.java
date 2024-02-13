694486ca9c4b60b158c4a1844c390c540dacefa2
Abdulbosit Rustamov <abdulbositrustamov399@gmail.com>
2024-02-13 11:11:58 +0500

    add MyBotServiceRus

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -152,6 +152,26 @@ public class MyBot extends TelegramLongPollingBot {
                     throw new RuntimeException(e);
                 }
             }
+            if (text.equals("kankulatorRus")){
+                try {
+                    execute(myBotServiceRus.kankulatorRus(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if (text.equals("✈\uFE0FВоздушный")){
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setChatId(chatId);
+                sendMessage.setText("Вес груза(кг): ");
+                DB.users.get(id).setCurrentPosition("kg");
+                DB.users.get(id).setSendType("Air");
+                try {
+                    execute(sendMessage);
+                } catch (Exception e) {
+                   e.printStackTrace();
+                }
+            }
             if(text.equals("✈\uFE0FHavo transporti")){
                 SendMessage sendMessage = new SendMessage();
                 sendMessage.setChatId(chatId);
