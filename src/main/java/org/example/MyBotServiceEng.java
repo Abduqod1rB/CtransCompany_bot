0b0a321e74811bbcbbb23db3ac9ab1a1001213e0
Abdulquddus Karimsherov <karimsherov@gmail.com>
2024-02-16 20:43:44 +0500

    Commit MyBotServiceEng tegilmasin

--- src/main/java/org/example/MyBotServiceEng.java
+++ src/main/java/org/example/MyBotServiceEng.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMediaGroup;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.media.InputMediaPhoto;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
@@ -561,5 +563,215 @@ public class MyBotServiceEng {
         sendMessage.setReplyMarkup(replyKeyboardMarkup);
 
         return sendMessage;
+    }
+
+    public SendPhoto bydSeal(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://www.ctrans.uz/storage/cars/GbnFoweKIIJ8SGRcR3X1qaBAmgizk9E8798rn536.webp"));
+        sendPhoto.setCaption("""
+                BYD Seal
+                
+                ✅Battery capacity, kW: 82,5       
+                ✅Acceleration 0-100 km, sec: 3,8  
+                ✅Power, kW/hp: 390/530            
+                ✅Drive unit: Full                 
+                ✅Range (NEDC), km: 650
+                ✅Number of seats: 5
+                ✅Maximum speed, km/h: 180
+                ✅Charging speed (slow / fast), hour: 11,8/0,5
+                
+                The BYD Seal is a compact electric executive sedan produced by BYD Auto. BYD Seal is built on e-Platform 3.0, BYD's new 800-volt automotive platform.
+                """);
+
+        return sendPhoto;
+    }
+
+    public SendPhoto bydHan(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://www.ctrans.uz/storage/cars/G5LEALmmX7YN3h2S0ejHxggKreWq0hgj0rkNV3zI.webp"));
+        sendPhoto.setCaption("""
+                BYD Han
+                
+                ✅Battery capacity, kW: 85,4    
+                ✅Acceleration 0-100 km, sec: 3,9
+                ✅Power, kW/hp: 380/517         
+                ✅Drive unit: Full                 
+                ✅Range (NEDC), km: 610
+                ✅Number of seats: 5
+                ✅Maximum speed, km/h: 185
+                ✅Charging speed (slow / fast), hour: 12,2/0,5
+                
+                BYD Han can boast a beautiful, stylish, distinctive and presentable look - a predatory front with narrow headlights and relief bumper, solid silhouette with a long hood, expressive sides and a small "tail" of the trunk, graceful stern with stylish lights.
+                """);
+
+        return sendPhoto;
+    }
+
+    public SendPhoto chevroletMonzaAtmo(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://www.ctrans.uz/storage/cars/7XZX6Aa66UBk54V6XkdOikpRj7MaSpDsfdPEecaw.webp"));
+        sendPhoto.setCaption("""
+                Chevrolet Monza 1.5L Atmo
+                
+                ✅Engine: 1.5 L (113 hp)    
+                ✅Transmission: 6-AT (Robot)
+                ✅Acceleration (0-100 km/h): 12.9 s         
+                ✅Fuel type: Petrol                 
+                ✅Drive unit: Full
+                ✅Number of seats: 5
+               
+                Chevrolet Monza sedan, new model 2023, front wheel drive. Gasoline engine - 1.5 liters, 113 horsepower, 6-speed automatic transmission.                
+                """);
+
+        return sendPhoto;
+    }
+
+    public SendPhoto chevroletMonzaTurbo(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://www.ctrans.uz/storage/cars/b3iKeMlJtqB2tNlBX4F9UYXbWLtrXRqJiD9ht8Sm.webp"));
+        sendPhoto.setCaption("""
+                Chevrolet Monza 1.3L Turbo
+                
+                ✅Engine: 1.3L Tubro (163 hp)    
+                ✅Transmission: 6-AT (Robot)
+                ✅Acceleration (0-100 km/h): 9.2 s         
+                ✅Fuel type: Petrol                 
+                ✅Drive unit: Full
+                ✅Drive unit: Full
+               
+                Chevrolet Monza sedan, new model 2023, front wheel drive. Gasoline engine - 1.3 liters (Turbo), 163 horsepower, 6-speed automatic transmission.            
+                """);
+
+        return sendPhoto;
+    }
+
+    public SendPhoto HondaCriderTurbo(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://www.ctrans.uz/storage/cars/WWWDezhpGgYHzzR9VApgwmj9eeNyNtW7OAlYygCk.webp"));
+        sendPhoto.setCaption("""
+                Honda Crider 1.0L Turbo
+                
+                ✅Engine capacity: 1.0 L Turbo   
+                ✅Engine's type: Petrol
+                ✅Consumption: 5.0 L     
+                ✅Power: 122 HP                 
+                ✅Acceleration to 100: 10.7 s
+               
+                Honda Crider is a C-class sedan, front-wheel drive. Petrol engines with a capacity of 122 horsepower.
+                """);
+
+        return sendPhoto;
+    }
+
+    public SendPhoto leapMotorC01(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://www.ctrans.uz/storage/cars/gAvBM75WivxQrmjUD4oBWpXvhS0MjkTfLEvz2v5d.webp"));
+        sendPhoto.setCaption("""
+                LeapMotor C01
+                
+                ✅Battery capacity, kW: 90   
+                ✅Acceleration 0-100 km, sec: 3,6
+                ✅Power, kW/hp: 400/544     
+                ✅Drive unit: Full                 
+                ✅Range (NEDC), km: 630
+                ✅Number of seats: 5
+                ✅Maximum speed, km/h: 180
+               
+                Leapmotor C01 Advance in its life is an electric car with Deluxe technology where there is an integration of innovative technologies for a soft and elegant appearance. Focus on high quality and satisfaction of various needs.
+                """);
+
+        return sendPhoto;
+    }
+
+    public SendPhoto HiPhiZ(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://www.ctrans.uz/storage/cars/TmaFh6qeILQV09RdIovwNghxCrx9BASiaMFvgdO9.webp"));
+        sendPhoto.setCaption("""
+                HiPhi Z
+                
+                ✅Battery capacity, kW: 120
+                ✅Acceleration 0-100 km, sec: 3,8
+                ✅Power, kW/hp: 494/672
+                ✅Drive unit: Full
+                ✅Power reserve (NEDC), km: 705
+                ✅Number of seats: 4
+                ✅Maximum speed, km/h: 200
+                ✅Charging speed (slow / fast), hour: 12,4/0,92               
+                
+                Welcome to the future of the automotive industry! Introducing the HiPhi Z, the epitome of cutting-edge technology and incomparable style that redefines the modern automotive dream. Combining elegance and innovation, HiPhi Z embodies a sophisticated design that is truly admirable at first sight. Harmoniously combining sleek, sleek lines with eye-catching details, this car is designed to grab attention on the road and remain the epitome of modern car design for years to come.
+                """);
+
+        return sendPhoto;
+    }
+
+    public SendPhoto imMotorsL7(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://www.ctrans.uz/storage/cars/n7IgJoqfWsM0w5ljA24sr0wBGUq7lrUp3g9bb5wi.webp"));
+        sendPhoto.setCaption("""
+                IM Motors L7
+                
+                ✅Battery capacity, kW: 90
+                ✅Acceleration 0-100 km, sec: 3,7
+                ✅Power: 445kW / 605Nm
+                ✅Drive unit: Full
+                ✅Power reserve, km: 615
+                ✅Number of seats: 5
+                ✅Maximum speed, km/h: 200             
+
+                Zhiji Auto is a brand jointly created by SAIC Group, Zhangjiang Hi-Tech and Alibaba Group. His Zhiji L7 is an all-electric executive sedan, size 3,100mm wheelbase and dual-engine system, 0-100km/h in just 3.9 seconds. The Zhiji L7 electric car has a length, width and height of 5098/1960/1485 mm and a wheelbase of 3100 mm. Drag coefficient 0.21.
+                """);
+
+        return sendPhoto;
+    }
+
+    public SendPhoto geometryG6(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://www.ctrans.uz/storage/cars/Zl6MhKhdsf1r5CfqLELkB4DnHjaliEh4XoTU6I56.webp"));
+        sendPhoto.setCaption("""
+                Geometry G6
+                
+                ✅Battery capacity, kW: 70
+                ✅Acceleration 0-100 km, sec: 6,9
+                ✅Power: 150kW / 204Nm
+                ✅Drive unit: Front
+                ✅Power reserve, km: 580
+                ✅Number of seats: 5
+                ✅Maximum speed, km/h: 150
+
+                The front of the car received a closed grille, and a glowing Geely Logo was installed under the hood. The main optics of the model has a sharp shape. Also of note is the use of hidden door handles and five-spoke rims. The G6 headlight group is irregularly shaped and connected by a black decorative stripe, forming a penetrating design.
+                """);
+
+        return sendPhoto;
+    }
+
+    public SendPhoto bmwi3eDrive40(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://www.ctrans.uz/storage/cars/eQXmyVoEXPqMbHTXPKK2SrtMvKeFYs9BkaYGTs70.webp"));
+        sendPhoto.setCaption("""
+                BMW i3 eDrive40
+                
+                ✅Battery capacity, kW: 78,92
+                ✅Acceleration 0-100 km, sec: 5,6
+                ✅Power, kW/hp: 250/340
+                ✅Drive unit: rear
+                ✅Power reserve (NEDC), km: 592
+                ✅Number of seats: 5
+                ✅Maximum speed, km/h: 180
+                ✅Charging speed (slow / fast), hour: 7,5/0,68
+                
+                The BMW i3 represents an uncompromising and attractive solution for the personal mobility requirements of the present and future in urban areas. The world's first premium car specifically designed to run entirely on electric power - and therefore emission-free.
+                """);
+
+        return sendPhoto;
     }
 }
