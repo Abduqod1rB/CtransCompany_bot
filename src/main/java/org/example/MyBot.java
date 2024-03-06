f12c41bbbd9fcc99afb2357c083e17e88023d06b
Abduqodir <abduqodirxojiakbarov202gmail.com>
2024-03-06 09:36:49 +0500

    add abu

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -8,8 +8,6 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-
 public class MyBot extends TelegramLongPollingBot {
     private final MyBotService myBotService = new MyBotService();
     private final MyBotServiceUz myBotServiceUz = new MyBotServiceUz();
